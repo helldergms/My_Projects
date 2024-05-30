49b652b0c6ea51fcc954bbdec2cba382fde0bb6e
helldergms <159969953+helldergms@users.noreply.github.com>
2024-05-30 03:05:49 -0300

    Create Ex007_Media-Nota-Aluno.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex007_Media-Nota-Aluno.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
+media = (nota1 + nota2) / 2
+print()
+print(f'A média entre as notas {nota1:.1f} e {nota2:.1f} deste aluno é {media:.1f}.')
+# Calculando média aritmética com notas escolares.
