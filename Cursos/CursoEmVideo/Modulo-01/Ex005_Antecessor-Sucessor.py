635a65275ebfd5aa729b7cb1318560c512cb332f
helldergms <159969953+helldergms@users.noreply.github.com>
2024-05-30 03:05:23 -0300

    Create Ex005_Antecessor-Sucessor.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex005_Antecessor-Sucessor.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+# antecessor = numero - 1
+# sucessor = numero + 1
+# print(f'O antecessor de {numero} é {antecessor} e o sucessor é {sucessor}.') // ^ Opção usando variáveis
+print(f'O antecessor de {numero} é {numero-1} e o sucessor é {numero+1}.') # < Opção sem variáveis
