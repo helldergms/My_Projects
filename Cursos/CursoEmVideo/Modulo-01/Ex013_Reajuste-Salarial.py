eb34e5f90cddb1f6f34ab1c36716adefef4de6a8
helldergms <159969953+helldergms@users.noreply.github.com>
2024-06-01 18:36:26 -0300

    Create Ex013_Reajuste-Salarial.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex013_Reajuste-Salarial.py
@@ -0,0 +1,5 @@
+salarioInicial = float(input("Informe o valor atual do salário que será reajustado: R$ "))
+reajuste = salarioInicial + (salarioInicial*0.15)
+print()
+print(f'O trabalhador informou salário atual de R${salarioInicial:.2f}')
+print(f'Com o reajuste de 15%, o salário deste trabalhador passará a ser de R${reajuste:.2f}')
