e6e300a06d969767ab6a4d4986b7d3e3523aed95
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-24 18:52:07 -0300

    Create Ex017_Catetos-Hipotenusa.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex017_Catetos-Hipotenusa.py
@@ -0,0 +1,5 @@
+from math import hypot
+cateto_oposto = float(input("Comprimento do cateto oposto: "))
+cateto_adjacente = float(input("Comprimento do cateto adjacente: "))
+hipotenusa = hypot(cateto_oposto, cateto_adjacente)
+print(f'A hipotenusa irá medir {hipotenusa:.2f}')
