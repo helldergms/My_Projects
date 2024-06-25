e9f046610ea2c5c24973248a6e976f4bcbae5fa0
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-25 17:51:33 -0300

    Create Ex022_Analisador-de-Texto.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex022_Analisador-de-Texto.py
@@ -0,0 +1,8 @@
+nome = str(input("Digite seu nome completo: ")).strip().title()
+dividido = nome.split()
+print("Analisando seu nome...")
+print(f'Seu nome completo com iniciais maiúsculas fica {nome}')
+print(f'Seu nome completo em maiúscula fica {nome.upper()}')
+print(f'Seu nome completo em minúscula fica {nome.lower()}')
+print(f'Seu nome completo possui {len(nome) - nome.count(' ')} letras no total')
+print(f'Seu primeiro nome é {dividido[0]} e possui {len(dividido[0])} letras')
