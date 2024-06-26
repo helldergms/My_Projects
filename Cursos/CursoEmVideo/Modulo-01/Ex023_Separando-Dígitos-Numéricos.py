22f8892cbf6d5193c56949c74022a07d64fedd76
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-25 21:13:38 -0300

    Create Ex023_Separando-Dígitos-Numéricos.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex023_Separando-Dígitos-Numéricos.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite um número de [0 a 9999]: "))
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
+print()
+print(f'Analisando o número {numero}')
+print(f'Unidades: {unidade}')
+print(f'Dezenas: {dezena}')
+print(f'Centenas: {centena}')
+print(f'Milhar: {milhar}')
