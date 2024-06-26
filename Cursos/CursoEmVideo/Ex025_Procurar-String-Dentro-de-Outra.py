25b323ae1e531c62c2db0083efe4dbb29a1aa4a0
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-25 23:06:54 -0300

    Deleted

--- Cursos/CursoEmVideo/Ex025_Procurar-String-Dentro-de-Outra.py
+++
@@ -1,4 +0,0 @@
-nome = str(input("Informe o seu nome completo: ")).strip().upper()
-lista_nome = nome.split()
-sobrenome = lista_nome.count('SILVA')
-print(f'Seu nome tem SILVA? {sobrenome == True}')
