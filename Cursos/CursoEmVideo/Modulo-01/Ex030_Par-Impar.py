2ca2f68daf7cde3c0e8a7f76f2199bc24fd22b89
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 15:17:42 -0300

    Create Ex030_Par-Impar.py

---
+++ Cursos/CursoEmVideo/Modulo-01/Ex030_Par-Impar.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número qualquer: "))
+
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR!")
+    print()
+else:
+    print(f"O número {numero} é ÍMPAR!")
+    print()
