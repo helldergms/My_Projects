3d89c9e7826a98b4b7eed7329651c2be1d51f482
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:16:53 -0300

    Update Ex004_Dissecando-Variavel.py

--- Cursos/CursoEmVideo/Modulo-01/Ex004_Dissecando-Variavel.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex004_Dissecando-Variavel.py
@@ -1,10 +1,12 @@
 teste = input("Digite algo: ")
+
 print()
-print(f'O tipo primitivo deste valor é {type(teste)}.')
-print(f'É alfanumérico? {teste.isalnum()}')
-print(f'É alfabético? {teste.isalpha()}')
-print(f'Está em minúsculo? {teste.islower()}')
-print(f'Está em maiúsculo? {teste.isupper()}')
-print(f'É um número? {teste.isnumeric()}')
-print(f'Só tem espaços? {teste.isspace()}')
-print(f'Está capitalizado?(primeira letra em maiúscula) {teste.istitle()}')
+
+print(f'O tipo primitivo deste valor é {type(teste)}.') # identificar o tipo da variável
+print(f'É alfanumérico? {teste.isalnum()}') # identificar se é alfanumérico
+print(f'É alfabético? {teste.isalpha()}') # identificar se é alfabético
+print(f'Está em minúsculo? {teste.islower()}') # identificar se está em minúsculo
+print(f'Está em maiúsculo? {teste.isupper()}') # identificar se está em maiúsculo
+print(f'É um número? {teste.isnumeric()}') # identificar se é um número
+print(f'Só tem espaços? {teste.isspace()}') # identificar se tem apenas espaços
+print(f'Está capitalizado?(primeira letra em maiúscula) {teste.istitle()}') # identificar se a primeira letra de cada palavra está em maiúscula
