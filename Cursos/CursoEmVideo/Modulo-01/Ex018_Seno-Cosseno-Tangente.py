25e9b2d11df76742bf9ed108a7054417a4125ab8
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:49 -0300

    Update Ex018_Seno-Cosseno-Tangente.py

--- Cursos/CursoEmVideo/Modulo-01/Ex018_Seno-Cosseno-Tangente.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex018_Seno-Cosseno-Tangente.py
@@ -1,8 +1,14 @@
 from math import radians, sin, cos, tan
+
+
 angulo = float(input("Informe o ângulo que deseja: "))
+
 seno = sin(radians(angulo))
+
 cosseno = cos(radians(angulo))
+
 tangente = tan(radians(angulo))
+
 print()
 print(f'O ângulo de {angulo} tem o SENO de {seno:.2f}')
 print(f'O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}')
