c1c74a86bc5a9c1f13e546780d712f806d4df19b
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:18 -0300

    Update Ex010_Conversor-Monetário.py

--- Cursos/CursoEmVideo/Modulo-01/Ex010_Conversor-Monetário.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex010_Conversor-Monetário.py
@@ -1,7 +1,12 @@
 real = float(input("Informe quantos reais você deseja fazer câmbio R$ "))
-cambioReal = real / 5.25
-print(f'Com R${real:.2f} você consegue comprar US${cambioReal:.2f}!')
+
+cambioReal = real / 5.25 # calcular câmbio do real para o dólar
+
+print(f'Com R$ {real:.2f} você consegue comprar US$ {cambioReal:.2f}!')
 print()
+
 dolar = float(input("Informe quantos dólares você deseja fazer câmbio US$ "))
-cambioDolar = dolar * 5.25
-print(f'Com US${dolar:.2f} você consegue comprar {cambioDolar:.2f}')
+
+cambioDolar = dolar * 5.25 # calcular câmbio do dólar para o real
+
+print(f'Com US$ {dolar:.2f} você consegue comprar R$ {cambioDolar:.2f}')
