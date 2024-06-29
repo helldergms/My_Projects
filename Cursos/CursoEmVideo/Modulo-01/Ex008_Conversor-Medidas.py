1bbfc71803c1166616b427f49c6d20fee19caa1b
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:12 -0300

    Update Ex008_Conversor-Medidas.py

--- Cursos/CursoEmVideo/Modulo-01/Ex008_Conversor-Medidas.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex008_Conversor-Medidas.py
@@ -1,10 +1,11 @@
 medida = int(input('Informe uma distância em metros: '))
+
 print()
-print(f'A medida de {medida:.1f}m corresponde a:\n'  # calculando medidas conforme apresentado abaixo
+print(f'A medida de {medida:.1f}m corresponde a:\n'
       f'\n'
-      f'{medida*0.001} kilômetro(s)\n'
-      f'{medida*0.01} hectômetro(s)\n'
-      f'{medida*0.1} decâmetro(s)\n'
-      f'{medida*10} decímetro(s)\n'
-      f'{medida*100} centímetro(s)\n'
-      f'{medida*1000} milímetro(s)')
+      f'{medida*0.001} kilômetro(s)\n' # calcular medida em kilômetro
+      f'{medida*0.01} hectômetro(s)\n' # calcular medida em hectômetro
+      f'{medida*0.1} decâmetro(s)\n' # calcular medida em decâmetro
+      f'{medida*10} decímetro(s)\n' # calcular medida em decímetro
+      f'{medida*100} centímetro(s)\n' # calcular medida em centímetro
+      f'{medida*1000} milímetro(s)') # calcular medida em milímetro
