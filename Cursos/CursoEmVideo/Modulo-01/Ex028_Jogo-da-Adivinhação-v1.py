43d0b321f6f383e8541cd76dbde282bbb54b940e
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:18:30 -0300

    Update Ex028_Jogo-da-Adivinhação-v1.py

--- Cursos/CursoEmVideo/Modulo-01/Ex028_Jogo-da-Adivinhação-v1.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex028_Jogo-da-Adivinhação-v1.py
@@ -1,16 +1,23 @@
 from random import randint
 from time import sleep
+
+
 game = " Jogo da Adivinhação "
+
 print(f'{"=" * 63}')
 print(game.center(63, '='))
 print(f'{"=" * 63}')
 print('Estou pensando em um número entre 0 e 5... Consegue adivinhar?')
+
 computador = randint(0, 5)
+
 jogador = int(input("Digite seu palpite: "))
+
 print()
 print("PROCESSANDO...")
 sleep(2.5)
 print()
+
 if jogador == computador:
     print("Parabéns, você ganhou!")
 
