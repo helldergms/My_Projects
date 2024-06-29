d9d50838371d3b75b5c91ced948feb52cbd973f0
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:18:08 -0300

    Update Ex023_Separando-Dígitos-Numéricos.py

--- Cursos/CursoEmVideo/Modulo-01/Ex023_Separando-Dígitos-Numéricos.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex023_Separando-Dígitos-Numéricos.py
@@ -1,8 +1,13 @@
 numero = int(input("Digite um número de [0 a 9999]: "))
+
 unidade = numero // 1 % 10
+
 dezena = numero // 10 % 10
+
 centena = numero // 100 % 10
+
 milhar = numero // 1000 % 10
+
 print()
 print(f'Analisando o número {numero}')
 print(f'Unidades: {unidade}')
