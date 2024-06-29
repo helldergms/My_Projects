11c4bda2c30c92f8384eb45e559e1aabee3c57ff
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:34 -0300

    Update Ex014_Conversor_Temperaturas_Celsius_Fahrenheit.py

--- Cursos/CursoEmVideo/Modulo-01/Ex014_Conversor_Temperaturas_Celsius_Fahrenheit.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex014_Conversor_Temperaturas_Celsius_Fahrenheit.py
@@ -1,3 +1,5 @@
 celsius = float(input("Informe a temperatura em ºC: "))
-fahrenheit = (celsius * 1.8) + 32
+
+fahrenheit = (celsius * 1.8) + 32 # cálculo para converter temperatura de Celsius para Fahrenheit
+
 print(f'A temperatura de {celsius}ºC equivale a {fahrenheit}ºF!')
