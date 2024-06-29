ed3bad91034073bc334eb8440091defcf8a1d91d
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:26 -0300

    Update Ex012_Calculando-Desconto.py

--- Cursos/CursoEmVideo/Modulo-01/Ex012_Calculando-Desconto.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex012_Calculando-Desconto.py
@@ -1,5 +1,7 @@
 produto = float(input("Informe o preço do produto que será aplicado o desconto: R$ "))
-desconto = produto - (produto*0.05)
+
+desconto = produto - (produto * 0.05) # calcular desconto de 5%
+
 print()
 print(f'O preço inicial do produto sem desconto é R${produto:.2f}\n'
       f'Aplicando o desconto de 5% o valor final será de R${desconto:.2f}')
