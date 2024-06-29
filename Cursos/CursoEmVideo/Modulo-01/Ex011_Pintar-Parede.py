a2d55b88df036ea663c98f5c3191aa83611853dd
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:21 -0300

    Update Ex011_Pintar-Parede.py

--- Cursos/CursoEmVideo/Modulo-01/Ex011_Pintar-Parede.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex011_Pintar-Parede.py
@@ -1,7 +1,11 @@
 largura = float(input("Informe a largura da parede em metros: "))
+
 comprimento = float(input("Agora informe a altura da parede em metros: "))
-área = largura * comprimento
-tinta = área / 2
+
+área = largura * comprimento # calcular área total de um espaço
+
+tinta = área / 2 # calcular quantidade de tinta que será usada no espaço
+
 print()
 print(f'Sua parede tem a dimensão de {largura:.2f}x{comprimento:.2f} e a área total é {área:.2f}m²')
 print(f'Para pintar essa parede você precisará de {tinta:.2f}L de tinta.')
