9e9cb90912c372b1d148c9a1b9849bb67b85f090
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:56 -0300

    Update Ex020_Sorteio-Ordem-Lista.py

--- Cursos/CursoEmVideo/Modulo-01/Ex020_Sorteio-Ordem-Lista.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex020_Sorteio-Ordem-Lista.py
@@ -1,11 +1,20 @@
 from random import shuffle
+
+
 print("ORDEM DE APRESENTAÇÃO DO TRABALHO ESCOLAR")
+
 aluno_1 = str(input("Informe o nome do primeiro aluno: "))
+
 aluno_2 = str(input("Informe o nome do segundo aluno: "))
+
 aluno_3 = str(input("Informe o nome do terceiro aluno: "))
+
 aluno_4 = str(input("Informe o nome do quarto aluno: "))
+
 lista_alunos = [aluno_1, aluno_2, aluno_3, aluno_4]
+
 shuffle(lista_alunos)
+
 print()
 print(f'A Ordem dos alunos que apresentarão os trabalhos será:')
 print(lista_alunos)
