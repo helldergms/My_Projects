8af861027273d202bb82159103b0415d44b42a8f
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:15 -0300

    Update Ex009_Tabuada_v1.py

--- Cursos/CursoEmVideo/Modulo-01/Ex009_Tabuada_v1.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex009_Tabuada_v1.py
@@ -1,13 +1,14 @@
 numero = int(input('Digite um número para ver sua respectiva tabuada: '))
+
 print('-'*12)
-print(f'{numero} x {1:2} = {numero*1}')
-print(f'{numero} x {2:2} = {numero*2}')
-print(f'{numero} x {3:2} = {numero*3}')
-print(f'{numero} x {4:2} = {numero*4}')
-print(f'{numero} x {5:2} = {numero*5}')
-print(f'{numero} x {6:2} = {numero*6}')
-print(f'{numero} x {7:2} = {numero*7}')
-print(f'{numero} x {8:2} = {numero*8}')
-print(f'{numero} x {9:2} = {numero*9}')
-print(f'{numero} x {10:2} = {numero*10}')
+print(f'{numero} x {1:2} = {numero*1}') # calcular tabuada do número multiplicado por 1
+print(f'{numero} x {2:2} = {numero*2}') # calcular tabuada do número multiplicado por 2
+print(f'{numero} x {3:2} = {numero*3}') # calcular tabuada do número multiplicado por 3
+print(f'{numero} x {4:2} = {numero*4}') # calcular tabuada do número multiplicado por 4
+print(f'{numero} x {5:2} = {numero*5}') # calcular tabuada do número multiplicado por 5
+print(f'{numero} x {6:2} = {numero*6}') # calcular tabuada do número multiplicado por 6
+print(f'{numero} x {7:2} = {numero*7}') # calcular tabuada do número multiplicado por 7
+print(f'{numero} x {8:2} = {numero*8}') # calcular tabuada do número multiplicado por 8
+print(f'{numero} x {9:2} = {numero*9}') # calcular tabuada do número multiplicado por 9
+print(f'{numero} x {10:2} = {numero*10}') # calcular tabuada do número multiplicado por 10
 print('-'*12)
