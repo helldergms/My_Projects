fbb7a1a79b4ff37a7c7fc7c6d1f79fc20506e070
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:01 -0300

    Update Ex006_Dobro-Triplo-RaizQdd.py

--- Cursos/CursoEmVideo/Modulo-01/Ex006_Dobro-Triplo-RaizQdd.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex006_Dobro-Triplo-RaizQdd.py
@@ -1,9 +1,12 @@
 numero = int(input('Informe um número: '))
+
 # dobro = numero * 2
 # triplo = numero * 3
 # raiz = numero ** (1/2)
-# print(f'O dobro de {numero} é {dobro}, o triplo é {triplo} e a raíz é {raiz:.2f}.') // ^ Opção usando variáveis.
-# v Opção sem usar variáveis.
+# print(f'O dobro de {numero} é {dobro}, o triplo é {triplo} e a raíz é {raiz:.2f}.') | Opção usando variáveis.
+
+
 print()
-print(f'O dobro de {numero} é {numero*2}.\nO triplo de {numero} é {numero*3}.\n'
+
+print(f'O dobro de {numero} é {numero*2}.\nO triplo de {numero} é {numero*3}.\n' # Opção sem usar variáveis.
       f'A raíz quadrada de {numero} é {numero**(1/2):.2f}.')
