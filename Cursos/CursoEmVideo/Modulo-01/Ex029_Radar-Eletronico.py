eec43baa3fd8f3a06077cb662f95b547c950e623
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 15:04:00 -0300

    Update Ex029_Radar-Eletronico.py

--- Cursos/CursoEmVideo/Modulo-01/Ex029_Radar-Eletronico.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex029_Radar-Eletronico.py
@@ -1,8 +1,8 @@
-velocidade_carro = int(input("Informe a velocidade registrada: "))
+velocidade_carro = float(input("Informe a velocidade registrada: "))
 limite_velocidade = 80
-multa = (velocidade_carro - limite_velocidade) * 7
 print()
 if velocidade_carro > limite_velocidade:
+    multa = (velocidade_carro - limite_velocidade) * 7
     print(f"Você ultrapassou o limite máximo permitido da via de 80 KM/h!")
     print(f'Seu veículo foi flagrado rodando a {velocidade_carro} KM/h e será multado em R$ {multa:.2f} pelo excedente de velocidade!')
     print()
@@ -10,4 +10,3 @@ if velocidade_carro > limite_velocidade:
 else:
     print("Veículo circulando dentro do limite permitido!")
     print()
-    
