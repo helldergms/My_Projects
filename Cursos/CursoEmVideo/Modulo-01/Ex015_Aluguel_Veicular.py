09f76c31a569dd68ae12255675a19e1f40045b46
Helder Gomes <159969953+helldergms@users.noreply.github.com>
2024-06-29 16:17:37 -0300

    Update Ex015_Aluguel_Veicular.py

--- Cursos/CursoEmVideo/Modulo-01/Ex015_Aluguel_Veicular.py
+++ Cursos/CursoEmVideo/Modulo-01/Ex015_Aluguel_Veicular.py
@@ -1,11 +1,16 @@
 diaria_aluguel = int(input('Informe a quantidade de dias para locação do veículo: '))
-valor_diaria = 60
-aluguel_veiculo = diaria_aluguel * valor_diaria
+
+valor_diaria = 60 # variável do preço da diária
+
+aluguel_veiculo = diaria_aluguel * valor_diaria # cálculo da diária da locação do veículo
 
 quilometros_rodados = float(input('Informe a quantidade de KM rodados: '))
-valor_quilometro = 0.15
-custo_quilometro = quilometros_rodados * valor_quilometro
-total = aluguel_veiculo + custo_quilometro
+
+valor_quilometro = 0.15 # variável da tarifa por quilômetro
+
+custo_quilometro = quilometros_rodados * valor_quilometro # cálculo do custo por quilômetro rodado
+
+total = aluguel_veiculo + custo_quilometro # cálculo do valor total a pagar
 
 print()
 print(f'Você alugou o veículo por {diaria_aluguel} dias e pagará R$ {aluguel_veiculo:.2f}')
